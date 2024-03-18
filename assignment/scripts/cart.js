c33b27883604f0de39b8706363da7a8979a7edcf
RobertsAlexis <155925135+RobertsAlexis@users.noreply.github.com>
2024-03-17 19:49:12 -0500

    Update cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,44 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
+console.log('basket')
 
+const maxItems = 5;
 
+function isFull(){
+    if(basket.length < maxItems){
+        return false;
+    }
+    return true;
+}
 
+function addItem(item) {
+    if (isFull() === false) {
+        basket.push(item)
+        return true;
+    }
+    return false;
+}
 
+function empty(){
+    while (basket.length !=0) {
+        basket.pop();
+    }
+}
+
+//adding and logging 3 items in basket
+
+console.log ('adding banana', addItem('banana'));
+console.log ('adding apple', addItem('apple'));
+console.log ('adding brocolli', addItem('brocolli'));
+
+function listItems() {
+    console.log(basket)
+}
+console.log('Items that are in my basket:', basket);
+
+console.log(`My basket has a ${basket}`);
 
 
 
